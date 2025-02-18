a5eb7db1e08b822a42dbae398c0d122525378c49
Elvea Huang <elvea.huang@gmail.com>
2025-02-18 13:46:06 +0800

    Initial commit

---
+++ platform-commons/commons-ai-starter/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    api(project(":platform-commons:commons-ai"))
+    api("org.springframework.ai:spring-ai-openai-spring-boot-starter")
+    api("org.springframework.ai:spring-ai-mcp-client-spring-boot-starter")
+}
+
+tasks.named<Jar>("jar") {
+    archiveBaseName.set("platform-commons-ai-starter")
+}
